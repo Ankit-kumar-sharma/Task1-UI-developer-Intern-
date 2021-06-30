b17e4eced55c1299d76408547fe9fc563b3b3327
Ankit Kumar Sharma <73944362+Ankit-kumar-sharma@users.noreply.github.com>
2021-06-30 11:32:56 +0530

    Add files via upload

--- emailvalidation.js
+++ emailvalidation.js
@@ -1,16 +1,19 @@
 function ValidateEmail(inputText)
 {
-var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-if(inputText.value.match(mailformat))
-{
-alert("Valid email address!");
-document.form1.text1.focus();
-return true;
-}
-else
-{
-    alert("Invalid email address");
+  var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if(inputText.value.match(mailformat))
+  {
+    document.getElementById('errdemo').innerHTML="Valid Email address";
+    document.getElementById('inbdemo').style.outlineColor="dodgerblue";
     document.form1.text1.focus();
-return false;
-}
+    return true;
+  }
+  else
+  {
+    document.getElementById('errdemo').innerHTML="!Please Insert valid Email address";
+    document.getElementById('inbdemo').style.outlineColor="red";
+    document.getElementById('inbdemo').style.border="2px solid red";
+    document.form1.text1.focus();
+    return false;   
+  }
 }
--- newpasswordvalidation.js
+++ newpasswordvalidation.js
@@ -1,22 +1,41 @@
-function verifyPassword() {  
-    var pw = document.getElementById("pswd").value;  
-    //check empty password field  
-    if(pw == "") {  
-       document.getElementById("message").innerHTML = "**Fill the password please!";  
-       return false;  
-    }  
-     
-   //minimum password length validation  
-    if(pw.length < 8) {  
-       document.getElementById("message").innerHTML = "**Password length must be atleast 8 characters";  
-       return false;  
-    }  
-    
-  //maximum length of password validation  
-    if(pw.length > 15) {  
-       document.getElementById("message").innerHTML = "**Password length must not exceed 15 characters";  
-       return false;  
-    } else {  
-       alert("Password is correct");  
-    }  
+function verifyPassword()
+{  
+  var pw = document.getElementById("pswd").value; 
+  var decimal=  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+  //check empty password field  
+  if(pw == "") 
+  {  
+     document.getElementById("message").innerHTML = "**Fill the password please!";  
+     document.getElementById("pswd").style.border="2px solid red"; 
+     return false;  
   }  
+  if(document.getElementById("pswd").value.match(decimal))
+  {
+     document.getElementById("message").innerHTML = "Password is correct";
+     return true;
+  }
+  else
+  {
+     //minimum password length validation  
+     if(pw.length < 8)
+     {  
+        document.getElementById("message").innerHTML = "**Password length must be atleast 8 characters";  
+        document.getElementById("pswd").style.border="2px solid red"; 
+        return false;  
+     }  
+    
+     //maximum length of password validation  
+     if(pw.length > 15) 
+     {  
+        document.getElementById("message").innerHTML = "**Password length must not exceed 15 characters";  
+        document.getElementById("pswd").style.border="2px solid red"; 
+        return false;  
+     } 
+      
+     document.getElementById("message").innerHTML = "**Password must contain atleast one lower letter,one upper,one numeric digit and one special char.";  
+     document.getElementById("pswd").style.border="2px solid red"; 
+     return false;  
+        
+   } 
+}  
+ 
